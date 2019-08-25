91fda6c7ee8b22b336fc43853bfee83922e87cc5
Bennisan <info@bennisan.be>
2019-08-25 02:05:27 +0200

    Styling Posts

--- packages/desktop/src/pages/home/home.js
+++ packages/desktop/src/pages/home/home.js
@@ -4,6 +4,7 @@ import Nav from '../../components/shared/nav'
 import Sidebar from '../../components/shared/sidebar'
 import Browser from '../../components/shared/browser';
 import Finance from '../../components/finance';
+import Post from '../../components/post';
 
 const Home = () => {
   const siderOpen = useSelector(state => state.navigation.siderOpen);
@@ -14,7 +15,7 @@ const Home = () => {
       </div>
       <div className="bottom flex">
         {
-          siderOpen ? <Sidebar /> : <Finance />
+          siderOpen ? <Sidebar /> : <Post />
         }
         <Browser />
       </div>
