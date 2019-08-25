b5342fc4cf82d0beaf1afd9563bbe56634b751cc
Bennisan <info@bennisan.be>
2019-08-25 03:52:14 +0200

    Stylings of the bids

--- packages/desktop/src/pages/home/home.js
+++ packages/desktop/src/pages/home/home.js
@@ -5,6 +5,7 @@ import Sidebar from '../../components/shared/sidebar'
 import Browser from '../../components/shared/browser';
 import Finance from '../../components/finance';
 import Bids from '../../components/bids';
+import Post from '../../components/post';
 
 const Home = () => {
   const siderOpen = useSelector(state => state.navigation.siderOpen);
